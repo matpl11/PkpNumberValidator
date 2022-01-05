4966c8f2e208dac8635615423b68a8b6fbdf4911
matpl11 <84911097+matpl11@users.noreply.github.com>
2022-01-05 16:20:01 +0100

    Dodanie sprawdzania całego zakresu

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,11 +1,15 @@
 ﻿Class MainWindow
-    Private Function CheckConstructionArea(o As Char)
+    Private Function CheckConstructionArea(o As Char, i As Integer)
         Dim e As Integer
         If (Short.TryParse(o, e)) Then
             If (e > 0 AndAlso e <= 9) Then
                 Return True
             Else
-                Return False
+                If (e = 0 AndAlso i = 1) Then 'Zero jest niedopuszczalne na początku, zwłaszcza w kontekście SWDR-a w TD2
+                    Return True
+                Else
+                    Return False
+                End If
             End If
         Else
             Return False
@@ -23,25 +27,54 @@
         Dim res As Boolean
 
         givenNumber = numberBox.Text
-        If givenNumber.Length <= 6 Then
-            For i = 0 To givenNumber.Length
-                If i = 0 Then
-                    res = CheckConstructionArea(givenNumber(i))
-                    If res = False Then
-                        ValidationResult(False)
-                        Return
-                    End If
+        For Each el As Char In givenNumber
+            If el = "0" Or el = "1" Or el = "2" Or el = "3" Or el = "4" Or el = "5" Or el = "6" Or el = "7" Or el = "8" Or el = "9" Then 'numer ma mieć tylko cyfry
+                If givenNumber.Length < 4 Then 'najkrótszy numer poc. ma 4 cyfry
+                    ValidationResult(False)
+                    Return
                 End If
-                If i = 1 Then
+                If givenNumber.Length <= 6 Then
+                    For i = 0 To givenNumber.Length
+                        If i <= 1 Then 'Sprawdzanie dwóch pierwszych cyfr (obszar konstrukcyjny)
+                            res = CheckConstructionArea(givenNumber(i), i)
+                            If res = False Then
+                                ValidationResult(False)
+                                Return
+                            End If
+                            If givenNumber(0) = "9" AndAlso givenNumber(1) = "9" Then 'Numer pociągu nie może zaczynać się na 99, rezerwa jest tylko jedna
+                                ValidationResult(False)
+                                Return
+                            End If
+                        Else
+                            If givenNumber.Length = 4 Then
+                                ValidationResult(True)
+                                Return
+                            ElseIf givenNumber.Length = 5 Then
+                                If givenNumber.Substring(2) = "000" Then 'numery 5-cyfrowe o końcówkach 001-999 są poprawne, końcówka 000 jest błędna
+                                    ValidationResult(False)
+                                    Return
+                                End If
+                            ElseIf givenNumber.Length = 6 Then
+                                If Integer.Parse(givenNumber.Substring(3)) > 899 Then 'Cyfry 4,5,6 dla numerów 6-cyfrowych z przedziału 900-999 stosuje się w przypadku konieczności zmiany numeru pociągu z uwagi na opóźnienie powyżej 24 h w celu uniknięcia dublowania się numerów pociągów.
+                                    ValidationResult(False)
+                                    Return
+                                End If
+                            End If
+                        End If
 
+                        If i = givenNumber.Length Then
+                            ValidationResult(True)
+                            Return
+                        End If
+                    Next
+                Else
+                    ValidationResult(False)
+                    Return
                 End If
-                If i = givenNumber.Length Then
-                    ValidationResult(True)
-                End If
-            Next
-        Else
-            ValidationResult(False)
-        End If
-
+            Else
+                ValidationResult(False)
+                Return
+            End If
+        Next
     End Sub
 End Class
